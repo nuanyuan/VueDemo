470189b4ffdcb86d665d2b448548246ff6dd8a12
nuanyuan <2458361819@qq.com>
2018-03-11 22:03:37 +0800

    Demo1

---
+++ Demo1/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+export default{
+  components:{
+    'v-header':header
+  }
+}
+</script>
+
+<style>
+  @import "common/stylus/mixin.styl";
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-1px(rgba(7,17,27,0.1))
+}
+.tab-item{
+  flex: 1;
+  text-align: center;
+}
+  .tab-item a{
+    display: block;
+    //text-decoration: none;
+    font-size: 14px;
+    color:rgb(77,85,93);
+  }
+  .tab-item a:active{
+    color: rgb(240,20,20);
+  }
+</style>
---
+++ Demo1/src/main.js
@@ -0,0 +1,40 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
+import './common/stylus/index.styl'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+let  app = Vue.extend(App)
+
+let  router = new VueRouter({
+  linkActiveClass:'active'
+})
+router.map({
+  '/goods':{
+    components:goods
+  },
+  '/ratings':{
+    component:ratings
+  },
+  'seller':{
+    component:seller
+  }
+
+
+})
+
+router.start(app,'#app')
+/* eslint-disable no-new */
+/*new Vue({
+  el:'body',
+  components:{App}
+})*/
+router.go('/goods')
